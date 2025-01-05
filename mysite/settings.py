c73b5202be7161c41693891c287fb8eebbc3dae2
miura hiroto <sanpuhiroto469@gmail.com>
2025-01-05 15:26:50 +0900

    Add WhiteNoise and STATIC_ROOT for static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ←これを追加
 ]
 
 ROOT_URLCONF = 'mysite.urls'
