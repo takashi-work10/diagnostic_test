702f05e2ec0dbd4ade2700b73feeea5707a21b62
miura hiroto <sanpuhiroto469@gmail.com>
2025-01-19 09:20:30 +0900

    up mysite.settings

--- api/index.py
+++ api/index.py
@@ -3,8 +3,8 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# Vercelはサーバーレスで環境変数が切り替わるため、DJANGO_SETTINGS_MODULEを手動でセット
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+#
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 # Djangoプロジェクトパスを追加する（configディレクトリ等を認識させるため）
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
