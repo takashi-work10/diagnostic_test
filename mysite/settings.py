96769b1c86ee3503c0d765674ff980e363c78eb4
miura hiroto <sanpuhiroto469@gmail.com>
2025-01-21 10:47:26 +0900

    mySQL add

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,8 +85,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        # ...
     }
 }
 # Password validation
