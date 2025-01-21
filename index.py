63bcd1f8ac7b1129331f85b3a2ccceecbbba9c12
miura hiroto <sanpuhiroto469@gmail.com>
2025-01-21 21:32:30 +0900

    rewrite index.py

--- index.py
+++ index.py
@@ -4,3 +4,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
+
+# Vercel が読み込めるよう、変数名を app にする
+app = application
